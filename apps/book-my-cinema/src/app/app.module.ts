fc59c0bfba9f64214415fba59eb08a23be8307f2
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-05-21 10:20:38 +0530

    Use shell module for book my cinema ap.

--- apps/book-my-cinema/src/app/app.module.ts
+++ apps/book-my-cinema/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DatabaseService, InMemoryDataService } from '@ticket-booking/ticket-booking-library';
 import { HttpClientModule } from '@angular/common/http';
@@ -13,6 +12,7 @@ import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { SharedModule, PipesModule } from '@ticket-booking/ticket-booking-library'
+import { ShellBookMyTicketModule } from '@ticket-booking/shell/book-my-cinema'
 
 @NgModule({
   declarations: [
@@ -21,7 +21,7 @@ import { SharedModule, PipesModule } from '@ticket-booking/ticket-booking-librar
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule,
+    ShellBookMyTicketModule,
     ModalModule.forRoot(),
     SharedModule,
     PipesModule,
--- libs/shell/book-my-cinema/src/lib/shell-book-my-cinema.module.ts
+++ libs/shell/book-my-cinema/src/lib/shell-book-my-cinema.module.ts
@@ -3,19 +3,20 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '@ticket-booking/movies/feature/home';
 import { MoviesComponent } from '@ticket-booking/movies/feature/movies';
+import { MovieDetailComponent } from '@ticket-booking/movies/feature/details';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'movies', component: MoviesComponent },
-  // { path: 'movie/:id', component: MovieDetailComponent }
+  { path: 'movie/:id', component: MovieDetailComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class ShellBookMyTicketModule { }
 
 @NgModule({
   imports: [CommonModule],
